4581ab8822ab8c56d60115ae10c48bdbc1b2f8ff
Neel <nkachalia1@gmail.com>
2026-01-11 14:29:54 -0500

    cleaned up UI

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
 from datetime import date
-from models import Task
-from storage import tasks, task_map
-from structures.stack import Stack
+
+from backend.models import Task
+from backend.storage import tasks, task_map
+from backend.structures.stack import Stack
 
 app = FastAPI()
 undo_stack = Stack()
 
 @app.post("/tasks")
 def add_task(title: str, priority: int, due_date: date):
-    task = Task(len(tasks)+1, title, priority, due_date)
+    task = Task(len(tasks) + 1, title, priority, due_date)
     tasks.append(task)
     task_map[task.id] = task
     return task
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
